3959096dc4a0556ce2ec12f355012483c0826df8
AytacSahin <haytacsahin@gmail.com>
2024-02-11 16:48:13 +0300

    CountryList module updated with filter

--- src/components/CountryList.tsx
+++ src/components/CountryList.tsx
@@ -1,33 +1,44 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import TextFilter from './TextFilter';
-
-const COUNTRIES_QUERY = gql`
-query {
-    countries {
-      code
-      name
-      languages {
-        code
-        name
-      }
-    }
-  }
-`;
+import React, { useState } from 'react';
+import { useCountryQuery } from '../services/CountryService';
 
 const CountryList: React.FC = () => {
-    const { loading, error, data } = useQuery(COUNTRIES_QUERY);
+    const { loading, error, data } = useCountryQuery();
+    const [searchQuery, setSearchQuery] = useState('');
+    const [groupBy, setGroupBy] = useState('');
 
+    // to do: try catch içine alabilirim sonra.
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error: {error.message}</p>;
 
-    const countries = data.countries;
-    console.log(countries);
+    const filteredCountries = data.countries
+        .filter((country: any) => country.name.toLowerCase().includes(searchQuery.toLowerCase()))
+        .filter((country: any) => groupBy === '' || country.languages.some((lang: any) => lang.name.toLowerCase().includes(groupBy.toLowerCase())));
+
+    const uniqueGroupByOptions: string[] = ["name", "code", "languages"];
 
     return (
         <div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Search"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <select
+                    value={groupBy}
+                    onChange={(e) => setGroupBy(e.target.value)}
+                >
+                    <option value="">Group By</option>
+                    {uniqueGroupByOptions.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </select>
+            </div>
             <ul>
-                {countries.map((country: any) => (
+                {filteredCountries.map((country: any) => (
                     <div style={{ backgroundColor: '#F0F0F0', marginBottom: '30px' }} key={country.code}>
                         <h2>Country Name: {country.name} </h2>
                         <h4>Code: {country.code}</h4>
