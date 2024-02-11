79796c89761cea8d762d46015ee241561db57044
AytacSahin <haytacsahin@gmail.com>
2024-02-11 15:20:15 +0300

    TextFilter module created

---
+++ src/components/TextFilter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TextFilter: React.FC = () => {
+    return (
+        <div>
+            <h1>Text Filter Bileşeni</h1>
+        </div>
+    );
+};
+
+export default TextFilter;
